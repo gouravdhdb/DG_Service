09194eee4f42675e65c6f1c75636baba78829ccb
Decken Gamer <replitgame@gmail.com>
2025-05-28 07:06:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
 
-  if (user === "admin" && pass === "1234") {
+  if (user === "admin" && pass === "admin1234") {
     localStorage.setItem("isAdmin", "true");
     window.location.href = "admin.html";
   } else {
